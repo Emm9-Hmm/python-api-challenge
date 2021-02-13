e30ad8961f2ede7f20b305c0723726f8a11e5b34
Emm9-Hmm <72900380+Emm9-Hmm@users.noreply.github.com>
2021-02-12 22:56:45 -0600

    problem solved

--- VacationPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
-
-# Google API Key
-g_key = "AIzaSyB7v1DlLkYIHc1CLbJjNjuykpt3M3l6S0k"
--- WeatherPy/api_keys.py
+++
@@ -1,5 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "ad4104cc7ebdc1a1cea27c6c31283ea4"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
